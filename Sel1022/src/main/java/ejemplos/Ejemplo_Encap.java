efe996a69ba11e2b0faa10b208214b810958fb99
Freddy <Freddy>
2022-11-13 22:25:29 -0300

    Freddy Subida de respaldo

---
+++ Sel1022/src/main/java/ejemplos/Ejemplo_Encap.java
@@ -0,0 +1,21 @@
+package ejemplos;
+
+public class Ejemplo_Encap{
+	
+	private int tipo;
+	
+	public void setTipo(int t) {// setear resultado
+		
+		tipo = t; 
+		
+	}
+	
+	public int getTipo() { // traer resultado
+		
+		return tipo;
+	}
+	
+
+
+
+}
--- Sel1022/src/main/java/selenium/Selenium_test.java
+++ Sel1022/src/main/java/selenium/Selenium_test.java
@@ -1,18 +1,41 @@
 package selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class Selenium_test {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		
 		String exePath = "./driver/chromedriver.exe"; // the folder esta dentro de la direccion que agregue 
 		System.setProperty("webdriver.chrome.driver", exePath); // le indica a la propiedad de de donde lo va a sacar
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
+		driver.get("https://www.google.com");  */
+	
+public static void main(String[] args) throws InterruptedException {
 		
+		String exePath = "./driver/chromedriver.exe"; // the folder esta dentro de la direccion que agregue 
+		System.setProperty("webdriver.chrome.driver", exePath); // le indica a la propiedad de de donde lo va a sacar
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://parabank.parasoft.com/"); 
+		
+		// Se declaran los objetos de login 
+		
+		WebElement inputUsername = driver.findElement(By.name("username"));
+		WebElement inputPassword = driver.findElement(By.name("password"));
+		WebElement btnLogin = driver.findElement(By.xpath("//input[@value = 'Log In']"));
+		
+		
+		Thread.sleep(2000);
+		inputUsername.sendKeys("cooltesters102022");
+		Thread.sleep(2000);
+		inputPassword.sendKeys("cooltesters102022");
+		Thread.sleep(2000);
+		btnLogin.click();
+		Thread.sleep(2000);
 		
 
 	}
